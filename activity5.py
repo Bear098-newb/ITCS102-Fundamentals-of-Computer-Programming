7ebc88f1babbd88ae75718358b9353bd91103089
Bear098-newb <ajgracio413@gmail.com>
2026-08-26 19:26:21 +0800

    Add files via upload
    activities and challenges today

---
+++ activity5.py
@@ -0,0 +1,12 @@
+a = 9
+b = 9
+
+print("variables testing")
+
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+print(a,"//",b,"=",a//b)
+print(a,"**",b,"=",a**b)
+print(a,"%",b,"=",a%b)
---
+++ code_challenge2.py
@@ -0,0 +1,32 @@
+bal = 19233
+print("bank balance:", bal )
+
+#computaion
+p1000 = bal//1000
+balance = bal%1000
+p500 = balance//500
+balance = balance%500
+p200 = balance//200
+balance = balance%200
+p100 = balance//100
+balance = balance%100
+p50 = balance//50
+balance = balance%50
+p20 = balance//20
+balance = balance%20
+p10 = balance//10
+balance = balance%10
+p5 = balance//5
+balance = balance%5
+p1 = balance//1
+balance = balance%1
+
+print("1000 = ",p1000)
+print("500 =",p500)
+print("200 = ",p200)
+print("100 =" ,p100)
+print("50 =",p50)
+print("20 =",p20)
+print("10 =",p10)
+print("5 =",p5)
+print("1 =",p1)
