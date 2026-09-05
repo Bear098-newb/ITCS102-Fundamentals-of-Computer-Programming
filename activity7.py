5cfdcd61353773b952f637f7112c7e98456c3c7f
Bear098-newb <ajgracio413@gmail.com>
2026-09-05 08:34:49 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,24 @@
+total = 0
+
+a = eval(input("input any number:"))
+total += a 
+b = eval(input("input any number:"))
+total += b
+c = eval(input("input any number:"))
+total += c 
+d = eval(input("input any number:"))
+total += d 
+e = eval(input("input any number:"))
+total += e 
+f = eval(input("input any number:"))
+total += f 
+g = eval(input("input any number:"))
+total += g 
+h = eval(input("input any number:"))
+total += h 
+i = eval(input("input any number:"))
+total += i 
+j = eval(input("input any number:"))
+total *= j 
+
+print("current amount ", total)
---
+++ activity8.py
@@ -0,0 +1,13 @@
+hobbies = ""
+
+f = input ("what are your hobbies?: ")
+hobbies += f + ", "
+
+f = input ("what else?: ")
+hobbies += f + ", "
+f = input ("what else?: ")
+hobbies += f + ", "
+f = input ("what else?: ")
+hobbies += f + ", "
+
+print("your hobbies are {", hobbies ,"}")
