c7ddaa447a6cf2de0ff53aaa400da9a22058a349
Bear098-newb <ajgracio413@gmail.com>
2026-09-10 17:01:02 +0800

    Refactor LBC code challenge for better readability
    Refactor input handling and cost calculation logic for clarity and accuracy.

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,32 +1,31 @@
-#code challenge 3 LBC
+print("======LBC=======")
+name = input("name: ")
+item_type = input("item type: ")
+fragile = input("is the item fragile?: ") 
+IsFragile = fragile in ["y", "yes"]
 
-name = input("sender name: ")
-item_type = input("type of item: ")
-fragile = input("fragile? (y/n): ")
-print()
-print("other information")
-weight = float(input("weight{kg}"))
-distance = float(input("distance{km}"))
-express = input("express? (y/n): ")
-international = input("international? (y/n): ")
+print("\n=====other information======")
 
+weight = float(input("weight in kg: "))
+distance = float(input("distance in km: "))
+express = input("is this express delivery?: ")
+isExpress = express in ["y", "yes"]
+international = input("is delivery from international: ")
+isInternational = international in ["y","yes"]
 base_cost = (weight*2.50)+(distance*0.15)
 total = base_cost
 
-if weight <= 2 or distance <= 100:
-    add = 0.00
-elif (express == "y" or express == "yes") and (international == "y" or international == "yes"):
-    add = (total * 1.40)+50
-elif express =="y" or express == "yes" or international == "y" or international == "yes":
+if weight <= 2 or distance <= 99:
+    add = 0.00, "free shipping"
+elif isExpress and isInternational:
+    add = (total*1.40)+50
+elif isExpress or isInternational:
     add = (total*1.20)+25
-elif weight > 30 or distance > 1000:
+elif weight >= 30 or distance >= 1000:
     add = total + 30
-elif express == "n" or express == "no" and international == "n" or international == "no":
+else:
     add = total
-else :
-    print("error")
 
 print()
-print("===========order overview==========")
-print("name of sender:",name, "\n type of item:", item_type)
-print("total cost: PHP",add)
+print("----overall review-----")
+print("sender:",name,"\nitem type: ",item_type,"\nDelivery fee: PHP", add)
