a22383a5acf2e541acc3494e2d537c0070f549c7
Bear098-newb <ajgracio413@gmail.com>
2026-09-11 16:27:30 +0800

    Refactor delivery cost calculation and input prompts

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,23 +1,26 @@
 print("======LBC=======")
 name = input("name: ")
 item_type = input("item type: ")
-fragile = bool(input("is the item fragile? True or False: "))
+fragile = bool(input("is the item fragile? (y/n): ") == "y")
 
 
 print("\n=====other information======")
 
 weight = float(input("weight in kg: "))
 distance = float(input("distance in km: "))
-isExpress = bool(input("is this express delivery? True or False: "))
-isInternational = bool(input("is delivery from international? True or False: "))
+express = bool(input("is this express delivery? (y/n): ") == "y") 
+international = bool(input("is delivery from international? (y/n): ") == "y") 
 base_cost = (weight*2.50)+(distance*0.15)
 total = base_cost
+isInternational = True
+isExpress = True
+
 
 if weight <= 2 or distance <= 99:
-    add = 0.00, "free shipping"
-elif isExpress == True and isInternational == True:
+    add = 0.00 , "FREE SHIPPING"
+elif express == True and international == True:
     add = (total*1.40)+50
-elif isExpress == True or isInternational == True:
+elif express == True or international == True: 
     add = (total*1.20)+25
 elif weight >= 30 or distance >= 1000:
     add = total + 30
@@ -27,6 +30,5 @@ else:
 print()
 print("----overall review-----")
 print()
-print("sender:",name,"\nitem type: ",item_type,"\nbase cost : PHP",total,"\ntotal cost: PHP", add)
-print()
-print("if the outcome is same please use True or False with a uppercase letter on ONLY the first letter")
+print("sender:",name,"\nitem type: ",item_type,"\nexpress:",express,"\ninternational:",international,"\nfragile: ", fragile,"\nbase cost : PHP",total,"\ntotal cost: PHP", add)
+print("-----end of review-----")
