fd40d51fa115011f65214f556e1c26fc14fb7bbb
Bear098-newb <ajgracio413@gmail.com>
2026-09-14 22:03:05 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,27 @@
+#activity 14
+
+age = eval(input("what is your age: "))
+is_employed = bool(input("are you employed (y/n): ") == "y")
+credit_score = eval(input("what is your credit score: "))
+annual_income = eval(input("what is your yearly income: "))
+has_collateral = bool(input("do you have a colateral? (y/n): ") == "y")
+
+
+if age >= 21 and is_employed:
+    if credit_score >= 750:
+        if annual_income >= 100000:
+            product = "APPROVED at 4.5% interest"
+        else:
+            product = "APPROVED at 5.0% interest"
+    elif credit_score > 600 and credit_score < 750:
+        if annual_income <= 40000:
+            product = "APPROVED at 9.5% interest"
+        elif has_collateral:
+            product = "APPROVED at 7.0% interest"
+        else :
+            product = "APPROVED at 8.0% interest"
+    else:
+        product = "rejected: credit score too low"
+else:
+    product = "DENIED: failed baseline criteria"
+print(product)
