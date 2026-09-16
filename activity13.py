0943090c253f8da05028ce7ca577ea2521202eaf
Bear098-newb <ajgracio413@gmail.com>
2026-09-16 10:28:29 +0800

    Refactor employment check to use boolean comparison

--- activity13.py
+++ activity13.py
@@ -1,13 +1,11 @@
-#activity 14
-
 age = eval(input("what is your age: "))
-is_employed = bool(input("are you employed (y/n): ") == "y")
+is_employed = bool(input("are you employed (y/n): "))
 credit_score = eval(input("what is your credit score: "))
 annual_income = eval(input("what is your yearly income: "))
-has_collateral = bool(input("do you have a colateral? (y/n): ") == "y")
+has_collateral = bool(input("do you have a colateral? (y/n): "))
 
 
-if age >= 21 and is_employed:
+if age >= 21 and is_employed == True:
     if credit_score >= 750:
         if annual_income >= 100000:
             product = "APPROVED at 4.5% interest"
@@ -16,7 +14,7 @@ if age >= 21 and is_employed:
     elif credit_score > 600 and credit_score < 750:
         if annual_income <= 40000:
             product = "APPROVED at 9.5% interest"
-        elif has_collateral:
+        elif has_collateral == True:
             product = "APPROVED at 7.0% interest"
         else :
             product = "APPROVED at 8.0% interest"
